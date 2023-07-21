29cdca6b854436b2e18cf7bdc64336c6dca5ac5d
Niortien <niortiencoulibaly2001@gmail.com>
2023-07-21 10:16:23 +0000

    e_commerce_v2

---
+++ shop.js
@@ -0,0 +1,28 @@
+let btnPlus=document.querySelector(".icon-plus")
+btnLess=document.querySelector(".icon-minus")
+let qty= document.querySelector(".icon-nil")
+btnPlus.addEventListener("click",increaseQuantity)
+btnLess.addEventListener("click",decreaseQuantity)
+
+function increaseQuantity(){
+   
+   qty.innerHTML=parseInt(qty.innerHTML)+1;
+   increase();
+}
+
+function decreaseQuantity(){
+   
+   if(qty.innerHTML>0){
+
+      qty.innerHTML=parseInt(qty.innerHTML)-1;
+   }
+}
+
+function increase(){
+   let price=document.querySelector(".price")
+   
+
+   price.innerHTML=parseInt=parseInt(price.innerHTML) *parseInt(qty.innerHTML);
+}
+
+
